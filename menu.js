61cfc6534b4c8c58b26d992a7320a541c6a64e9b
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2021-11-08 15:40:59 +0100

    Add hide menu when clicked on link

--- menu.js
+++ menu.js
@@ -1,8 +1,10 @@
 /*  eslint linebreak-style: ["error", "windows"]    */
+/*  eslint linebreak-style: ["error", "unix"]    */
 
 const hamburgerItem = document.querySelector('.hamburger-item');
 const closeItem = document.querySelector('.close-item');
 const hamburgerMenu = document.querySelector('.hamburger-menu');
+const menuItem = document.querySelectorAll('.menu-item');
 
 closeItem.addEventListener('click', () => {
   hamburgerMenu.classList.remove('hamburger-menu');
@@ -13,3 +15,12 @@ hamburgerItem.addEventListener('click', () => {
   hamburgerMenu.classList.remove('hidden-on-small');
   hamburgerMenu.classList.add('hamburger-menu');
 });
+
+menuItem.forEach(
+  (menuItem) => {
+    menuItem.addEventListener('click', () => {
+      hamburgerMenu.classList.remove('hamburger-menu');
+      hamburgerMenu.classList.add('hidden-on-small');
+    });
+  },
+);
