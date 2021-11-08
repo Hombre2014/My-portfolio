53d0d0e46807b1edcf821313b8dac7020f5248e6
Furkan Denizhan <84407337+nevisende@users.noreply.github.com>
2021-11-08 16:35:53 +0300

    Completed mobile menu

--- menu.js
+++ menu.js
@@ -1,3 +1,15 @@
-let hamburgerItem = document.querySelector('.hamburger-item');
-let closeItem = document.querySelector('.close-item');
-let hamburgerMenu = document.querySelector('.hamburger-menu');
+/*  eslint linebreak-style: ["error", "windows"]    */
+
+const hamburgerItem = document.querySelector('.hamburger-item');
+const closeItem = document.querySelector('.close-item');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+closeItem.addEventListener('click', () => {
+  hamburgerMenu.classList.remove('hamburger-menu');
+  hamburgerMenu.classList.add('hidden-on-small');
+});
+
+hamburgerItem.addEventListener('click', () => {
+  hamburgerMenu.classList.remove('hidden-on-small');
+  hamburgerMenu.classList.add('hamburger-menu');
+});
