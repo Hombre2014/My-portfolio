c0afec9c6318ba3883d8ab997230b52c1267ffef
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2021-11-08 14:23:11 +0100

    Add menu.js file and add classes

---
+++ menu.js
@@ -0,0 +1,3 @@
+let hamburgerItem = document.querySelector('.hamburger-item');
+let closeItem = document.querySelector('.close-item');
+let hamburgerMenu = document.querySelector('.hamburger-menu');
